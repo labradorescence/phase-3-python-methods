dc6a7a9ed1568ff0e537b35d6bb66c9a3cdc9184
labradorescence <orangenewyorkcreativeagency@gmail.com>
2022-07-26 15:22:50 -0400

    practice

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
+from xml.dom.minidom import TypeInfo
+
+
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    return num1 + num2
     pass
 
 def halve(number):
+    if isinstance(number, int):
+        return number/2
+    elif isinstance(number, float):
+        return number/2
+    elif isinstance(number, complex):
+        return number/2
+    return None
     pass
